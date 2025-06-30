090f57af82359013e1c1c10fd129060f79ba0842
yuwnloy <yuwnloyblog@163.com>
2025-06-30 18:03:47 +0800

    upd

--- emailengines/emailengine.go
+++ emailengines/emailengine.go
@@ -1,5 +1,15 @@
 package emailengines
 
+var (
+	DefaultEmailEngine IEmailEngine = &NilEmailEngine{}
+)
+
 type IEmailEngine interface {
 	SendMail(toAddress string, subject string, body string) error
+}
+
+type NilEmailEngine struct{}
+
+func (engine *NilEmailEngine) SendMail(toAddress string, subject string, body string) error {
+	return nil
 }
